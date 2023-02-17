501fe372b784eff7af98dd736c78cc2c5bae05a2
lukemodena <97751351+lukemodena@users.noreply.github.com>
2023-02-17 13:59:22 +0000

    modified:   src/Components/common/miscObjects.js

--- src/Components/common/miscObjects.js
+++ src/Components/common/miscObjects.js
@@ -203,7 +203,7 @@ export const phoneHandler = (Phone) => {
         )
     } else {
         return (
-            <a href={`tel:${Phone}`}><BsFillTelephoneFill style={{height:"100%", width:"100%"}}/></a>
+            <a href={`tel:${Phone}`}><BsFillTelephoneFill style={{height:"50%", width:"50%"}}/></a>
         )
     }
 };
